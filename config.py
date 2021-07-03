7291074289f3b49d37aea59e38251eb397693635
teknowafel <39500700+teknowafel@users.noreply.github.com>
2021-07-03 16:21:15 -0400

    a

---
+++ config.py
@@ -0,0 +1,19 @@
+
+class config:
+    siteName = "Example Website"
+    siteCopyright = "Copyright (c) 2021 Example, Inc. All rights reserved."
+
+    header = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>%s</title>
+    </head>
+    """
+
+    footer = """
+    <footer>
+        <p>%s</p>
+    </footer>
+    </html>
+    """
