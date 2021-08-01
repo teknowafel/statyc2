b2b97e5842b9c6e435129fcc2670c98f18719dd8
teknowafel <39500700+teknowafel@users.noreply.github.com>
2021-08-01 15:15:07 -0400

    fix font sizes, fix script

--- main.sh
+++ main.sh
@@ -1,8 +1,4 @@
 EXPORT THEME_NAME=default
 
-tmux new -s http -d
-tmux send-keys -t http "python -m http.server" ENTER
 rm -r docs/
-wget -r -np -nH localhost:8000/themes/$THEME_NAME/ -P docs/
-cat file | sed '/<script>/,/<\/script>/d'
-tmux kill-session -t http
+cp -R themes/$THEME_NAME/* docs/
