91e0a3992356219cb1e96cae9773cbbc2e9922b6
teknowafel <39500700+teknowafel@users.noreply.github.com>
2021-08-01 13:27:39 -0400

    the thing does the thing and it works yes!

--- main.sh
+++ main.sh
@@ -4,4 +4,5 @@ tmux new -s http -d
 tmux send-keys -t http "python -m http.server" ENTER
 rm -r docs/
 wget -r -np -nH localhost:8000/themes/$THEME_NAME/ -P docs/
+cat file | sed '/<script>/,/<\/script>/d'
 tmux kill-session -t http
