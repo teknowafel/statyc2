aabe998ae53ac0f6a38b614dd78358d516cb089f
teknowafel <39500700+teknowafel@users.noreply.github.com>
2021-08-09 20:26:55 -0400

    plenty of progress for today; taking a break

--- themes/default/module/templates.js
+++ themes/default/module/templates.js
@@ -24,22 +24,6 @@ var templates = {
         var post = postHandler.getPostByName(postName);
         return postHandler.renderFullPost(post);
     },
-    landing: function(){
-        var post = {
-            name: "Welcome!",
-            author: "teknowafel",
-            date: 1628550611000,
-            featuredImage: "none",
-            content: `Statyc-py rewritten. Again. Statyc2 was the first rewrite of statyc-py, 
-            and the functionality is still pretty good. However, my haste to produce something 
-            working left the code an unreadable mess. That's what this is for! Statyc3 or 2.5
-            if you will, this is the the next *(and hopefully for me, last)* revision of statyc.
-            Here are some changes since the last rewrite:
-            \n* Views
-                - Now, instead of clunky separate html pages, we simply have views that can be rendered using the ?view= parameter in the URL.`
-        }
-        return postHandler.renderFullPost(post);
-    },
     view: views.renderViews(),
 };
 
